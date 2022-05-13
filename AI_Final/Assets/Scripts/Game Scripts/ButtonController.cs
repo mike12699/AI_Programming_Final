128de28069b140572dd1ef3cf5ee664d4ed50a65
mike12699 <mariomania85@gmail.com>
2022-05-12 19:05:37 -0500

    Edited player movement

--- AI_Final/Assets/Scripts/Game Scripts/ButtonController.cs
+++ AI_Final/Assets/Scripts/Game Scripts/ButtonController.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
+    public GameObject titlePanel;
+    public GameObject helpPanel;
+
     public void ResetGame()
     {
         SceneManager.LoadScene("TitleScreen");
@@ -13,5 +16,17 @@ public class ButtonController : MonoBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void OpenOptions()
+    {
+        titlePanel.SetActive(false);
+        helpPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        titlePanel.SetActive(true);
+        helpPanel.SetActive(false);
     }
 }
